e91657d5b5916b2fa90aee174639153c4afbf013
Alexandre Brasil <alexandre.l.brasil@gmail.com>
2025-02-06 12:41:28 -0300

    Tornando a conexão ao banco mais robusta contra erros

--- infra/database.js
+++ infra/database.js
@@ -9,14 +9,23 @@ async function query(queryObject) {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  console.log("Credenciais de conexão: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   try {
+    await client.connect();
     const result = await client.query(queryObject); // ---> passando o SQL como parametro da função
     // const result = await client.query("SELECT $1::text as message", ["Hello world!",]); ---> passando o SQL direto como string
     //console.log(result.rows[0].message); ---> mostrando o resultado da primeira linha
     return result;
   } catch (error) {
     console.error("Erro ao conectar com o banco de dados: ", error);
+    throw error;
   } finally {
     await client.end();
   }
