1221025cff87e949eb27a8e507a4498d5b018af0
Alexandre Brasil <alexandre.l.brasil@gmail.com>
2025-03-05 17:41:07 -0300

    add `api/v1/migrations` endpoints

--- infra/database.js
+++ infra/database.js
@@ -2,17 +2,9 @@ import pg from "pg";
 const { Client } = pg;
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-
+  let client;
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject); // ---> passando o SQL como parametro da função
     // const result = await client.query("SELECT $1::text as message", ["Hello world!",]); ---> passando o SQL direto como string
     //console.log(result.rows[0].message); ---> mostrando o resultado da primeira linha
@@ -25,8 +17,25 @@ async function query(queryObject) {
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  // Exportar deste modo com os nomes identicos (query: query, getNewClient: getNewClient,)
+  //  se torna redundante e pode ser feito como está apos os comentarios
+  query,
+  getNewClient,
 };
 
 function getSSLValues() {
@@ -35,5 +44,5 @@ function getSSLValues() {
       ca: process.env.POSTGRES_CA,
     };
   }
-  process.env.NODE_ENV === "development" ? false : true;
+  process.env.NODE_ENV === "production" ? true : false;
 }
