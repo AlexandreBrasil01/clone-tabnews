db1d701b36c097be560f0223248237da7ff8b216
Alexandre Brasil <alexandre.l.brasil@gmail.com>
2025-03-19 17:18:12 -0300

    fix `dbClient` connections opened

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -36,5 +36,6 @@ export default async function migrations(request, response) {
     return response.status(200).json(migratedMigrations);
   }
 
+  dbClient.end();
   return response.status(405).end();
 }
