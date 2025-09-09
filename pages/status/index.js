3a327ffe309530cb3ccc004616e9516c4181e077
Alexandre Brasil <alexandre.l.brasil@gmail.com>
2025-09-09 17:30:48 -0300

    refactor: optimize `/status` code

--- pages/status/index.js
+++ pages/status/index.js
@@ -11,9 +11,6 @@ export default function StatusPage() {
     <>
       <h1>Status</h1>
       <UpdatedAt />
-      <VersionDB />
-      <MaxConnectionsDB />
-      <OpenedConnectionsDB />
     </>
   );
 }
@@ -22,45 +19,22 @@ function UpdatedAt() {
   const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
     refreshInterval: 2000,
   });
-  let updatedAtText = "Carregando...";
+
+  let statusText = "Carregando...";
+
   if (!isLoading && data) {
-    updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+    let updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
+    let versinDBText = data.dependencies.database.version;
+    let maxConnText = data.dependencies.database.max_connections;
+    let numConnText = data.dependencies.database.opened_connections;
+    statusText = (
+      <>
+        <div>Última atualização: {updatedAtText}</div>
+        <div>Versão do Banco de dados: {versinDBText}</div>
+        <div>N° máximo de conexões: {maxConnText}</div>
+        <div>N° de conexões abertas: {numConnText}</div>
+      </>
+    );
   }
-  return <div>Última atualização: {updatedAtText}</div>;
-}
-
-function VersionDB() {
-  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
-    refreshInterval: 2000,
-  });
-
-  let versinDBText = "Carregando...";
-  if (!isLoading && data) {
-    versinDBText = data.dependencies.database.version;
-  }
-  return <div>Versão do Banco de dados: {versinDBText}</div>;
-}
-
-function MaxConnectionsDB() {
-  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
-    refreshInterval: 2000,
-  });
-
-  let maxConnText = "Carregando...";
-  if (!isLoading && data) {
-    maxConnText = data.dependencies.database.max_connections;
-  }
-  return <div>N° máximo de conexões: {maxConnText}</div>;
-}
-
-function OpenedConnectionsDB() {
-  const { isLoading, data } = useSWR("/api/v1/status", fetchAPI, {
-    refreshInterval: 2000,
-  });
-
-  let numConnText = "Carregando...";
-  if (!isLoading && data) {
-    numConnText = data.dependencies.database.opened_connections;
-  }
-  return <div>N° de conexões abertas: {numConnText}</div>;
+  return <div>{statusText}</div>;
 }
