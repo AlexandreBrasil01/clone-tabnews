0d60e603b6249a20a2dfec15968d01c579ec23cb
Alexandre Brasil <alexandre.l.brasil@gmail.com>
2025-10-22 17:07:33 -0300

    refactor: make `/api/v1/migrations` use `migrator` model

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,59 +1,22 @@
 import { createRouter } from "next-connect";
-import migrationRunner from "node-pg-migrate";
-import { resolve } from "node:path";
-import database from "infra/database.js";
 import controller from "infra/controller.js";
+import migrator from "models/migrator.js";
 
 const router = createRouter();
 
 router.get(getHandler);
 router.post(postHandler);
-
 export default router.handler(controller.errorHandlers);
 
-//Configurações padrão para a execução
-const defaultMigrationOptions = {
-  dryRun: true,
-  dir: resolve("infra", "migrations"),
-  direction: "up",
-  verbose: true,
-  migrationsTable: "pgmigrations",
-};
-
 async function getHandler(request, response) {
-  let dbClient;
-
-  try {
-    //Cria o client para a conexão com o banco de dados
-    dbClient = await database.getNewClient();
-
-    const pendingMigrations = await migrationRunner({ ...defaultMigrationOptions, dbClient });
-    return response.status(200).json(pendingMigrations);
-  } finally {
-    await dbClient.end();
-  }
+  const pendingMigrations = await migrator.listPendingMigrations();
+  return response.status(200).json(pendingMigrations);
 }
 
 async function postHandler(request, response) {
-  let dbClient;
-  try {
-    //Cria o client para a conexão com o banco de dados
-    dbClient = await database.getNewClient();
-
-    const migratedMigrations = await migrationRunner({
-      //Caso precise alterar algo das config padrao basta utilizar os ... que sao chamados de spread operator
-      // depois passar o atributo que deseja alterar e o novo valor
-      ...defaultMigrationOptions,
-      dbClient,
-      dryRun: false,
-    });
-
-    if (migratedMigrations.length > 0) {
-      return response.status(201).json(migratedMigrations);
-    }
-
-    return response.status(200).json(migratedMigrations);
-  } finally {
-    await dbClient.end();
+  const migratedMigrations = await migrator.runPendingMigrations();
+  if (migratedMigrations.length > 0) {
+    return response.status(201).json(migratedMigrations);
   }
+  return response.status(200).json(migratedMigrations);
 }
